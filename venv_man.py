c49d905f3c0ccfe3817322c8dd6d38e6662e82c1
Ben Glick <glick@glick.cloud>
2021-01-19 19:59:59 -0800

    yapf

--- venv_man.py
+++ venv_man.py
@@ -1,5 +1,4 @@
 #!/local/cluster/bin/python
-
 """
 venv_man.py
 
@@ -14,56 +13,80 @@ import subprocess
 import os
 import collections
 
+
 class Config:
     def __init__(self):
         self.BASE_VENV_PATH = "/Users/ben/bread/venv"
+
 
 config = Config()
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--list",
+    parser.add_argument("-l",
+                        "--list",
                         action="store_true",
                         default=False,
                         help="List all available virtual environments.")
-    parser.add_argument("-n", "--name",
+    parser.add_argument("-n",
+                        "--name",
                         type=str,
                         default=None,
                         help="Specify the name of a virtual environment")
-    parser.add_argument("-c", "--create",
-                        action="store_true",
-                        default=False,
-                        help="Create a new virtual environment with name specified")
-    parser.add_argument("-d", "--delete",
-                        action="store_true",
-                        default=False,
-                        help="Delete the virtual environment with name specified")
-    parser.add_argument("-a", "--add",
-                        type=str,
-                        default=None,
-                        nargs="+",
-                        help="Add specified python packages to specified virtual environmentwith pip.")
-    parser.add_argument("-r", "--remove",
-                        type=str,
-                        default=None,
-                        nargs="+",
-                        help="Remove specified python packages from specified virtual environment with pip.")
-    parser.add_argument("-e", "--activate",
-                        action="store_true",
-                        default=False,
-                        help="Print the command to activate (engage) a virtual environment")
-    parser.add_argument("-s", "--show",
-                        action="store_true",
-                        default=False,
-                        help="Show a list of packages installed in specified environment")
-    parser.add_argument("-y", "--yes",
+    parser.add_argument(
+        "-c",
+        "--create",
+        action="store_true",
+        default=False,
+        help="Create a new virtual environment with name specified")
+    parser.add_argument(
+        "-d",
+        "--delete",
+        action="store_true",
+        default=False,
+        help="Delete the virtual environment with name specified")
+    parser.add_argument(
+        "-a",
+        "--add",
+        type=str,
+        default=None,
+        nargs="+",
+        help=
+        "Add specified python packages to specified virtual environmentwith pip."
+    )
+    parser.add_argument(
+        "-r",
+        "--remove",
+        type=str,
+        default=None,
+        nargs="+",
+        help=
+        "Remove specified python packages from specified virtual environment with pip."
+    )
+    parser.add_argument(
+        "-e",
+        "--activate",
+        action="store_true",
+        default=False,
+        help="Print the command to activate (engage) a virtual environment")
+    parser.add_argument(
+        "-s",
+        "--show",
+        action="store_true",
+        default=False,
+        help="Show a list of packages installed in specified environment")
+    parser.add_argument("-y",
+                        "--yes",
                         action="store_true",
                         default=False,
                         help="Force remove, do not ask for confirmation.")
     args = parser.parse_args()
 
-    if not any([args.list, args.activate, args.remove, args.add, args.delete, args.create, args.show]):
+    if not any([
+            args.list, args.activate, args.remove, args.add, args.delete,
+            args.create, args.show
+    ]):
         print("ERROR: No action specified.")
         sys.exit(1)
 
@@ -89,10 +112,12 @@ def parse_args():
         sys.exit(1)
 
     if any([args.create, args.delete]) and any([args.add, args.remove]):
-        print("ERROR: Add/Remove and Create/Delete are not supported together.")
+        print(
+            "ERROR: Add/Remove and Create/Delete are not supported together.")
         sys.exit(1)
 
-    if args.show and any([args.activate, args.remove, args.add, args.delete, args.create]):
+    if args.show and any(
+        [args.activate, args.remove, args.add, args.delete, args.create]):
         print("ERROR: Show cannot be specified with other actions")
         sys.exit(1)
 
@@ -113,6 +138,7 @@ def parse_args():
 
     return args
 
+
 def list_venvs():
     """
     List all virtual environments in directory `config.BASE_VENV_PATH`
@@ -128,6 +154,7 @@ def list_venvs():
         if _venv_exists(directory):
             print(f"\t{directory}")
 
+
 def remove_virtualenv(name, force):
     """
     Delete a virtual environment with name :param name.
@@ -135,10 +162,13 @@ def remove_virtualenv(name, force):
     if not os.path.isdir(_dir_path(name)):
         print(f"ERROR: could not find environment {name}")
         sys.exit(1)
-    if not force and input(f"Are you sure you want to delete virtualenv {name}? y/N ").lower() != "y":
+    if not force and input(
+            f"Are you sure you want to delete virtualenv {name}? y/N ").lower(
+            ) != "y":
         print("Aborting.")
         sys.exit(0)
     subprocess.check_call(["rm", "-rf", _dir_path(name)])
+
 
 def create_virtualenv(name):
     """
@@ -147,7 +177,10 @@ def create_virtualenv(name):
     if os.path.exists(_dir_path(name)):
         print(f"ERROR: name {name} already taken.")
         sys.exit(1)
-    subprocess.check_call(" ".join(["cd", config.BASE_VENV_PATH, ";", "virtualenv", name]), shell=True)
+    subprocess.check_call(" ".join(
+        ["cd", config.BASE_VENV_PATH, ";", "virtualenv", name]),
+                          shell=True)
+
 
 def show_activate_cmd(name):
     """
@@ -158,6 +191,7 @@ def show_activate_cmd(name):
         sys.exit(1)
     print(f"To activate the virtualenv {name}, use the following command:")
     print(f"\tsource {_dir_path(name)}/bin/activate")
+
 
 def add_pkgs(pkgs, name):
     """
@@ -171,6 +205,7 @@ def add_pkgs(pkgs, name):
         cmd_base += f"pip install {pkg};"
     cmd_base += "deactivate"
 
+
 def remove_pkgs(pkgs, name):
     """
     Remove packages :param pkgs from virtualenv :param name
@@ -183,12 +218,16 @@ def remove_pkgs(pkgs, name):
         cmd_base += f"pip uninstall {pkg};"
     cmd_base += "deactivate"
 
+
 def _dir_path(name):
     return f"{config.BASE_VENV_PATH}/{name}"
 
+
 def _venv_exists(name):
     dir_path = f"{config.BASE_VENV_PATH}/{name}"
-    return os.path.isdir(dir_path) and os.path.exists(f"{dir_path}/bin/activate")
+    return os.path.isdir(dir_path) and os.path.exists(
+        f"{dir_path}/bin/activate")
+
 
 if __name__ == '__main__':
     args = parse_args()
